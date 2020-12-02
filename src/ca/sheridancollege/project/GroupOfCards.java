e5e693ba5b5b507da59d417c45b90f9bebc73d6e
Mu Zhang <zhang106@sheridancollege.ca>
2020-12-02 15:38:12 -0500

    assign color and value to each card of the deck

--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -9,14 +9,44 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 /**
- * A concrete class that represents any grouping of cards for a Game. HINT, you might want to subclass this more than
- * once. The group of cards has a maximum size attribute which is flexible for reuse.
- *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
+ * The Deck class consists of 108 Uno cards. There are 4 suits: red, green, blue, yellow
+ * each consisting of one 0 card, two 1s, 2s...8s, 9s cards; two Draw Two cards; two Skip cards; and two Reverse cards.
+ * in addition, there are four Wild cards and four Wild Draw Four cards.
  */
 public class GroupOfCards {
 
+        private UnoCard[] cards;
+        //tracking how many cards in the deck
+        private int cardsInDeck;
+
+    public GroupOfCards() {
+        cards = new UnoCard[108];
+    }
+    //build a deck of 108 cards and assigning specific color and value to each of them
+    public void buildCards(){
+        //put 4 colors into this array
+         UnoCard.Color[] colors = UnoCard.Color.values();
+         cardsInDeck = 0;
+         //this forloop used to assign value 0-9 to each color(red, green, blue and yellow 
+         for(int i=0; i<colors.length-1; i++){
+                 UnoCard.Color color = colors[i];
+                 //making 0 cards for each color, bcz 0 is a single card
+                 cards[cardsInDeck++] = new UnoCard(color, UnoCard.Value.getValue(0));
+                 //assign 1-9 to every color
+                 for(int j=1;j<10;j++){
+                     //double them
+                     cards[cardsInDeck++] = new UnoCard(color, UnoCard.Value.getValue(j));
+                     cards[cardsInDeck++] = new UnoCard(color, UnoCard.Value.getValue(j));
+                 }
+                 
+                 
+                 }
+}
+    
+        
+    
+    
+    
     //The group of cards, stored in an ArrayList
     private ArrayList<Card> cards;
     private int size;//the size of the grouping
